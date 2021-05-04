946bd255d71ac47e6c6127eab519681cd6ed761e
SATTDN21.01.06 <sattdn21.01.06@logigear.com>
2021-05-04 13:51:18 +0700

    add code at TC01 to TC02

--- src/main/java/common/Common.java
+++ src/main/java/common/Common.java
@@ -1,22 +1,10 @@
 package common;
 
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Common {
-    public static JSONArray getJsonData(String pathFileJson) throws IOException, ParseException {
-        JSONParser jsonParser = new JSONParser();
-        FileReader reader = new FileReader(pathFileJson);
-        Object obj = jsonParser.parse(reader);
-        return (JSONArray) obj;
-    }
 
     public static String plusDayFromNow(long daysToAdd, String formatDay) {
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern(formatDay);
