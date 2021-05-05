f914199c6b5b94cf00a91497be328a9bd7a422bb
SATTDN21.01.06 <SATTDN21.01.06@logigear.com>
2021-05-05 10:25:23 +0700

    second update test case 01

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,14 +3,16 @@ package tests;
 import common.Constant;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import page_objects.HomePage;
 import page_objects.LoginPage;
 
 public class LoginTest extends BaseTest {
     LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
 
     @Test(description = "User can log into Railway with valid username and password")
     public void TC001() {
-        loginPage.gotoLoginPage();
+        homePage.gotoLoginPage();
         loginPage.login(Constant.USERNAME, Constant.PASSWORD);
 
         String expectedWelcomeMsg = "Welcome " + Constant.USERNAME;
