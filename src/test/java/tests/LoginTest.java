50e457275aa8c7018eea3627dbacb68c4464ca50
SATTDN21.01.06 <SATTDN21.01.06@logigear.com>
2021-05-05 10:29:56 +0700

    first update test case 02

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,21 +3,20 @@ package tests;
 import common.Constant;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import page_objects.HomePage;
 import page_objects.LoginPage;
 
 public class LoginTest extends BaseTest {
     LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
 
     @Test(description = "User can't login with blank Username text box")
     public void TC002() {
-        loginPage.gotoLoginPage();
+        homePage.gotoLoginPage();
 
-        String blankUsername = "";
-        loginPage.login(blankUsername, Constant.PASSWORD);
+        String expectedWelcomeMsg = "Welcome " + Constant.USERNAME;
+        String actualWelcomeMsg = loginPage.getWelcomeMessage();
 
-        String expectedErrorMsg = "There was a problem with your login and/or errors exist in your form.";
-        String actualErrorMsg = loginPage.getErrorMsg();
-
-        Assert.assertEquals(actualErrorMsg, expectedErrorMsg, "Error message is incorrect.");
+        Assert.assertEquals(actualWelcomeMsg, expectedWelcomeMsg, "Welcome message is incorrect.");
     }
 }
