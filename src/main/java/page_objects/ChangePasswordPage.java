26b7d58729d4ced2132f564548589ee59976ee83
SATTDN21.01.06 <SATTDN21.01.06@logigear.com>
2021-05-06 13:57:47 +0700

    Write test script TC09

--- src/main/java/page_objects/ChangePasswordPage.java
+++ src/main/java/page_objects/ChangePasswordPage.java
@@ -1,4 +1,53 @@
 package page_objects;
 
+import common.helpers.BrowserHelper;
+import common.helpers.ElementHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class ChangePasswordPage extends BasePage {
+
+    //Locators
+    private final By txtCurrentPassword = By.id("currentPassword");
+    private final By txtNewPassword = By.id("newPassword");
+    private final By txtConfirmPassword = By.id("confirmPassword");
+    private final By btnChangePassword = By.cssSelector("input[title='Change password']");
+    private final By lblSuccessfulMsg = By.className("success");
+
+    //Elements
+    private WebElement getTxtCurrentPassword() {
+        return BrowserHelper.getDriver().findElement(txtCurrentPassword);
+    }
+
+    private WebElement getTxtNewPassword() {
+        return BrowserHelper.getDriver().findElement(txtNewPassword);
+    }
+
+    private WebElement getTxtConfirmPassword() {
+        return BrowserHelper.getDriver().findElement(txtConfirmPassword);
+    }
+
+    private WebElement getBtnChangePassword() {
+        return BrowserHelper.getDriver().findElement(btnChangePassword);
+    }
+
+    private WebElement getLblSuccessfulMsg() {
+        return BrowserHelper.getDriver().findElement(lblSuccessfulMsg);
+    }
+
+    //Methods
+
+    public void changePassword(String currentPassword, String newPassword, String confirmPassword) {
+        ElementHelper.scrollToView(getBtnChangePassword());
+
+        getTxtCurrentPassword().sendKeys(currentPassword);
+        getTxtNewPassword().sendKeys(newPassword);
+        getTxtConfirmPassword().sendKeys(confirmPassword);
+
+        getBtnChangePassword().click();
+    }
+
+    public String getSuccessfulMsg() {
+        return getLblSuccessfulMsg().getText().trim();
+    }
 }
---
+++ src/test/java/tests/ChangePasswordTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import models.Account;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page_objects.ChangePasswordPage;
+import page_objects.LoginPage;
+import page_objects.RegisterPage;
+
+public class ChangePasswordTest extends BaseTest {
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+    RegisterPage registerPage = new RegisterPage();
+    ChangePasswordPage changePasswordPage = new ChangePasswordPage();
+
+    @Test(description = "User can change password")
+    public void TC009() {
+        homePage.gotoRegisterPage();
+        Account account = new Account();
+        registerPage.register(account);
+
+        registerPage.gotoLoginPage();
+
+        String email = account.getEmail();
+        String password = account.getPassword();
+
+        loginPage.login(email, password);
+
+        homePage.gotoChangePasswordPage();
+
+        String newPassword = "123456789";
+        changePasswordPage.changePassword(password, newPassword, newPassword);
+
+        String expectedSuccessfulMsg = "Your password has been updated!";
+        String actualSuccessfulMsg = changePasswordPage.getSuccessfulMsg();
+
+        Assert.assertEquals(actualSuccessfulMsg, expectedSuccessfulMsg, "Successful message is incorrect");
+    }
+}
